1bf5c876e8fac740fcfb7a7a2eba06453935f3e1
brandonscottred <brandon.redford@outlook.com>
2025-01-23 08:35:32 -0700

    build asteroids

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_clock = pygame.time.Clock()
+    update_group = pygame.sprite.Group()
+    draw_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    Player.containers = (update_group, draw_group)
+    Asteroid.containers = (asteroid_group, update_group, draw_group)
+    AsteroidField.containers = (update_group)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in update_group:
+            obj.update(dt)
         screen.fill((0,0,0))
+        for obj in draw_group:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = my_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
